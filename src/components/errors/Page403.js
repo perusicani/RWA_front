1315227ec1c330c06ca8449df09cdd0d59c13ded
Ani <perusicani@gmail.com>
2022-02-21 21:39:49 +0100

    - Playing with styling

--- src/components/errors/Page403.js
+++ src/components/errors/Page403.js
@@ -1,17 +1,19 @@
 import React from "react";
 
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+
+import { Link } from 'react-router-dom';
+
 function Page403() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <div className="card card-body">
-                        <h1>Page 403 | Forbidden</h1>
-                        <h3>Access denied! You are not an admin.</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Container flex='true'>
+            <Card style={{ marginTop: 50, marginBottom: 50, padding: 50 }} >
+                <h1>Page 403 | Forbidden</h1>
+                <h3>Access denied! You are not an admin.</h3>
+                <Link className='btn btn-primary' to='/'>Back to home :(</Link>
+            </Card>
+        </Container>
     );
 }
 
--- src/components/errors/Page404.js
+++ src/components/errors/Page404.js
@@ -1,17 +1,19 @@
 import React from "react";
 
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+
+import { Link } from 'react-router-dom';
+
 function Page404() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-6">
-                    <div className="card card-body">
-                        <h1>Page 404 | Not Found</h1>
-                        <h3>Url / Page you are seaching was not found.</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Container flex='true'>
+            <Card style={{ marginTop: 50, marginBottom: 50, padding: 50 }} >
+                <h1>Page 404 | Not Found</h1>
+                <h3>Url / Page you are seaching was not found.</h3>
+                <Link className='btn btn-primary' to='/'>Back to home :(</Link>
+            </Card>
+        </Container>
     );
 }
 
